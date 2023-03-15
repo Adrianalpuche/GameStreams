30bd39a3cf76d4cd77f18bbc5ecdea209e347153
Adrian Alpuche <marco.phonix04>
2023-03-14 18:33:24 -0600

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Adrián Alpuche on 14/03/23.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
