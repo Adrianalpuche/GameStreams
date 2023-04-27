248960adb1b790c36b2352d90b6110d12a8ed880
Adrian Alpuche <marco.phonix04>
2023-04-27 17:47:26 -0600

    GameStream

--- GameStream/ContentView.swift
+++ GameStream/ContentView.swift
@@ -8,19 +8,231 @@
 import SwiftUI
 
 struct ContentView: View {
+  
+    var body: some View {
+        NavigationView{
+            ZStack{
+                Color(red: 19/255, green: 30/255, blue: 53/255).ignoresSafeArea()
+                VStack{
+                    Image("appLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 250, height: 100, alignment: .center)
+                    
+                    //El.fit hace que se acomode la imagen a la pantalla de manera bonita y ajustada
+                    
+                    //Se acomoda con .frame la imagen en el centro
+                    
+                    LoginAndRegisterView()
+                    
+                }
+                
+            } .navigationBarHidden(true)
+        }
+        }
+    }
+
+  
+
+  
+
+struct LoginAndRegisterView: View{
+    @State var typeLogin = false //La priemra vez que se inicie sesion va a mostrar por default el login
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack{
+                Spacer()
+                Button("INICIA SESIÓN"){
+                    typeLogin = true
+                    print("Pantalla de inicio de sesión")
+                }.foregroundColor(typeLogin ? .white : .gray)
+                Spacer()
+                Button("REGISTRATE"){
+                    typeLogin = false
+                    print("Pantalla de registro")
+                }.foregroundColor(typeLogin ? .gray : .white)
+                Spacer()
+            }
+
+            Spacer()
+            if typeLogin == true {
+                LoginView()
+            } else{
+                RegisterView()
+            }
         }
-        .padding()
+    }
+}
+
+  
+
+struct LoginView: View {
+    @State var email=""
+    @State var password=""
+    @State var show = true
+    @State var isHomeScreenActive = false
+    
+    var body: some View {
+        ScrollView{
+            VStack {
+                //Text("LoginView").foregroundColor(Color("dark-cian"))
+                Text("Correo electronico").foregroundColor(Color("dark-cian"))
+                ZStack(alignment: .leading){
+                    if email.isEmpty{
+                        Text(verbatim:"example@example.com").font(.caption).foregroundColor(.gray)
+                    }
+                    TextField("", text:$email)
+                }
+                
+                Divider().frame(height: 1).background(Color("dark-cian")).padding(.bottom)
+                
+                Text("Contraseña").foregroundColor(.white)
+                
+                ZStack(alignment: .leading){
+                    if password.isEmpty{
+                        Text(verbatim: "Escribe tu contraseña").font(.caption).foregroundColor(.white)
+                    }
+                    if show{
+                        TextField("", text:$password).foregroundColor(.white)
+                    }
+                    else{
+                        SecureField("",text:$password).foregroundColor(.white)
+                    }
+                    HStack{
+                        Spacer()
+                    Button(action: {
+                        show.toggle()
+                    }) {
+                        Image(systemName: self.show ? "eye" : "eye.slash").accentColor(.gray)}
+
+                }
+            }
+                
+                Divider().frame(height: 1).background(Color("dark-cian")).padding(.bottom)
+                Text("¿Olvidaste tu contraseña?").font(.footnote).frame(width: 300,alignment: .trailing).foregroundColor(Color("dark-cian"))
+                
+                Button(action: login, label: {
+                    Text("Iniciar Sesion").fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity,alignment: .center).padding(EdgeInsets(top:11,leading: 18,bottom: 11,trailing: 18)).overlay(RoundedRectangle(cornerRadius: 6.0).stroke(Color ("dark-cian"),lineWidth: 1.0).shadow(color: .white, radius: 6))
+                }).navigationDestination(isPresented: $isHomeScreenActive, destination:{Home()})
+                
+            }
+        }.padding(.horizontal, 77)
+        
+        NavigationLink(
+        destination: Home(),
+        isActive: $isHomeScreenActive,
+        label: {
+        EmptyView()
+        })
+    }
+    func login(){
+        isHomeScreenActive = true
+    }
+    
+    
+  
+}
+
+struct RegisterView: View {
+    @State var email=""
+    @State var password=""
+    @State var confirm=""
+    @State var show = true
+    var body: some View {
+        
+        ScrollView{
+            VStack {
+                VStack{
+                    Spacer()
+                    Text("Elige una foto de perfil").fontWeight(.bold).foregroundColor(.white)
+                    Text("Puedes cambiar o eligirla mas tarde").lineLimit(1)
+                        .foregroundColor(.gray)
+                    ZStack{
+                        Spacer()
+                        Image("foto-perfil").resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width:300 ,height:100 )
+                            .clipShape(Circle())
+                        
+                        Button(action: {}){Image(systemName: "camera")               .resizable().aspectRatio(contentMode: .fit)
+                            .foregroundColor(.white)}.frame(width: 35,height: 35)
+                    }
+                    VStack(alignment: .leading){
+                        Spacer()
+                        Spacer()
+                        Spacer()
+                        Text("Correo electronico").foregroundColor(Color ("dark-cian"))
+                        ZStack(alignment: .leading){
+                            if email.isEmpty{
+                                Text(verbatim:"ejemplo@gmail.com").font(.caption).foregroundColor(.gray)
+                            }
+                            TextField("", text:$email)
+                        }
+                        Divider().frame(height: 1).background(Color("dark-cian")).padding(.bottom)
+                        Text("Contraseña").foregroundColor(.white)
+                        
+                        ZStack(alignment: .leading){
+                            if password.isEmpty{
+                                Text(verbatim: "Escribe una contraseña").font(.caption).foregroundColor(.white)
+                            }
+                            SecureField("", text:$password).foregroundColor(.white)
+                        }
+                        
+                        Divider().frame(height: 1).background(.white).padding(.bottom)
+                        
+                        VStack(alignment: .leading){
+                            Text("Confirmar contraseña").foregroundColor(.white)
+                            
+                            ZStack(alignment: .leading){
+                                if confirm.isEmpty{
+                                    Text(verbatim: "Escribe otra vez la contraseña ").font(.caption).foregroundColor(.white)
+                                }
+                                SecureField("", text:$confirm).foregroundColor(.white)
+                            }
+                            
+                            Divider().frame(height: 1).background(.white).padding(.bottom)
+                            Button(action: register, label: {
+                                Text("Registrate").font(.system(size:23)).fontWeight(.bold)
+                                    .foregroundColor(.white)
+                                    .frame(maxWidth: .infinity,alignment: .center).padding(EdgeInsets(top:11,leading: 18,bottom: 11,trailing: 18)).overlay(RoundedRectangle(cornerRadius: 6.0).stroke(Color ("dark-cian"),lineWidth: 1.0).shadow(color: .white, radius: 6))
+                            })
+                        }
+                    }.padding(.horizontal, 20)
+                    Spacer()
+                    Spacer()
+                    Text("Registrate con redes sociales").foregroundColor(.white)
+                    HStack{
+                        Button {
+                            
+                        } label: {
+                            Text("Facebook").foregroundColor(.white)
+                                .frame(width: 75,alignment: .center).padding(EdgeInsets(top:10,leading: 18,bottom: 10,trailing: 18))
+                        } .background(Color(red: 40/255, green: 55/255, blue: 88/255)).cornerRadius(9)
+                        
+                        Button{
+                            
+                        }label: {
+                            Text("Twitter").foregroundColor(.white) .frame(width: 75,alignment: .center).padding(EdgeInsets(top:10,leading: 18,bottom: 10,trailing: 18))
+                        }.background(Color(red: 40/255, green: 55/255, blue: 88/255)).cornerRadius(9)
+
+                    }
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+
     static var previews: some View {
+
         ContentView()
+
     }
+
+}
+
+
+
+func register(){
+    print("Estoy registrandome")
 }
---
+++ GameStream/Home.swift
@@ -0,0 +1,258 @@
+//
+//  Home.swift
+//  GameStream
+//
+//  Created by Adrián Alpuche on 18/04/23.
+//
+
+import SwiftUI
+import AVKit
+
+
+struct Home: View {
+    @State var selectedTab:Int = 2
+   
+    var body: some View {
+        
+        
+        TabView(selection: $selectedTab) {
+        Text("ProfileScreen").font(.system(size: 30,weight: .bold, design: .rounded))
+        
+        .tabItem{
+
+        Image(systemName: "person")
+
+        Text("Perfil")
+
+        }.tag(0)
+
+        Text("GamesScreen").font(.system(size: 30,weight: .bold, design: .rounded))
+
+        .tabItem{
+
+        Image(systemName: "gamecontroller")
+
+        Text("Juegos")
+
+        }.tag(1)
+
+           HomeScreen()
+            
+        .tabItem{
+
+        Image(systemName: "house")
+
+        Text("Inicio")
+
+        }.tag(2)
+
+        Text("FavoritesScreen").font(.system(size: 30,weight: .bold, design: .rounded))
+        .tabItem{
+        Image(systemName: "heart")
+        Text("Favoritos")
+        }.tag(3)
+            
+            
+        }.accentColor(.white)
+    }
+    init(){
+
+    UITabBar.appearance().backgroundColor = UIColor(Color("TabBar-Color"))
+
+    UITabBar.appearance().isTranslucent = true
+
+    UITabBar.appearance().unselectedItemTintColor = UIColor.gray
+
+    }
+}
+
+struct HomeScreen:View {
+    @State var searchText = ""
+    var body: some View{
+        ZStack {
+            Color("Marine").ignoresSafeArea()
+            VStack {
+                Image("appLogo").resizable().aspectRatio(contentMode: .fit).frame(width: 250).padding(.horizontal, 11.0)
+                HStack{
+                    Button(action: search, label: {
+                        Image(systemName: "magnifyingglass").foregroundColor(searchText.isEmpty ? Color(.yellow) : Color("dark-cian"))
+                    })
+                    TextField(text: $searchText,
+                    label: {
+                    Text("Buscar un video").foregroundColor(.gray)
+                    }).foregroundColor(.white)
+                }.padding([.top,.leading,.bottom],11.0).background(Color("blue-gray")).clipShape(Capsule())
+                ScrollView(showsIndicators: false){ SubModuleHome() }
+            }.padding(.horizontal,18)
+            
+        }.navigationBarHidden(true).navigationBarBackButtonHidden(true)
+        
+    }
+    func search () {
+    print("El usuario está buscando \(searchText)")
+    }
+}
+
+
+struct SubModuleHome:View {
+    //URL default
+    @State var url = "https://cdn.cloudflare.steamstatic.com/steam/apps/256658589/movie480.mp4"
+    //To not activate the player by default
+    @State var isPlayerActive = false
+    //All videos that we want to show
+    let urlVideos:[String] = ["https://cdn.cloudflare.steamstatic.com/steam/apps/256658589/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256671638/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256720061/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256814567/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256705156/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256801252/movie480.mp4","https://cdn.cloudflare.steamstatic.com/steam/apps/256757119/movie480.mp4"]
+    var body: some View {
+        VStack{
+            Text("Los más populares").font(.title3).foregroundColor(.white).bold().frame(minWidth: 0, maxWidth: .infinity, alignment: .leading).padding(.top)
+            NavigationLink {
+                VideoPlayer(player: AVPlayer(url: URL(string: url)!)).frame(width: 420, height: 360, alignment: .center)
+            } label: {
+                ZStack{
+                    Button {
+                        url = urlVideos[0]
+                        print("URL: \(url)")
+                        isPlayerActive = true
+                    } label: {
+                        VStack(spacing: 0){
+                            Image("The Witcher 3")
+                                .resizable()
+                                .scaledToFill()
+                            Text("The Witcher 3")
+                                .foregroundColor(.white)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .background(Color("blue-gray"))
+                        }
+                    }
+                    Image(systemName: "play.circle.fill")
+                        .resizable()
+                        .foregroundColor(.white)
+                        .frame(width: 42, height: 42)
+                }.frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                    .padding(.vertical)
+            }
+        }.frame(minWidth: 0, maxWidth: .infinity, alignment: .center).padding(.vertical)
+        Text("Categorías sugeridas para ti").font(.title3).foregroundColor(.white).bold().frame(minWidth: 0, maxWidth: .infinity,alignment: .leading)
+
+        ScrollView(.horizontal,showsIndicators: false){
+        HStack{
+            
+        Button(action: {}, label: {
+        ZStack{
+        RoundedRectangle(cornerRadius: 8)
+        .fill(Color("blue-gray"))
+        .frame(width: 160, height: 90)
+        Image("FPS")
+        .resizable()
+        .scaledToFit()
+        .frame(width: 42, height: 42)
+        }
+        })
+            
+        Button(action: {}, label: {
+        ZStack{
+        RoundedRectangle(cornerRadius: 8)
+        .fill(Color("blue-gray"))
+        .frame(width: 160, height: 90)
+        Image("RPG").resizable().scaledToFit().frame(width: 42, height: 42)
+        }
+        })
+            
+        Button(action: {}, label: {
+        ZStack{
+        RoundedRectangle(cornerRadius: 8)
+        .fill(Color("blue-gray"))
+        .frame(width: 160, height: 90)
+        Image("OpenWorld").resizable().scaledToFit().frame(width: 42, height: 42)
+            }
+        })
+        }
+        }
+
+        //Carrusel
+
+        Text("Recomendados para ti").font(.title3).foregroundColor(.white).bold().frame(minWidth: 0, maxWidth: .infinity,alignment: .leading)
+
+        ScrollView(.horizontal, showsIndicators: false){
+
+        HStack{
+        NavigationLink {
+        VideoPlayer(player: AVPlayer(url: URL(string: urlVideos[1])!)).frame(width: 420, height: 360, alignment: .center)
+
+        } label: {
+
+        Image("Abzu")
+        .resizable()
+        .scaledToFit()
+        .frame(width: 240, height: 135)
+
+        }
+        NavigationLink {
+        VideoPlayer(player: AVPlayer(url: URL(string: urlVideos[2])!)).frame(width: 420, height: 360, alignment: .center)
+        } label: {
+        Image("Crash bandicoot")
+        .resizable()
+        .scaledToFit()
+        .frame(width: 240, height: 135)
+        }
+
+        NavigationLink {
+        VideoPlayer(player: AVPlayer(url: URL(string: urlVideos[3])!)).frame(width: 420, height: 360, alignment: .center)
+        } label: {
+        Image("Death Stranding")
+        .resizable()
+        .scaledToFit()
+        .frame(width: 240, height: 135)
+        }
+        }
+        }
+
+        //Carrusel
+
+        Text("Videojuegos que podrían gustarte").font(.title3).foregroundColor(.white).bold().frame(minWidth: 0, maxWidth: .infinity,alignment: .leading)
+
+        ScrollView(.horizontal, showsIndicators: false){
+
+        HStack{
+        NavigationLink {
+        VideoPlayer(player: AVPlayer(url: URL(string: urlVideos[6])!)).frame(width: 420, height: 360, alignment: .center)
+        } label: {
+        Image("Grand Theft Auto V")
+        .resizable()
+        .scaledToFit()
+        .frame(width: 240, height: 135)
+        }
+
+        NavigationLink {
+        VideoPlayer(player: AVPlayer(url: URL(string: urlVideos[5])!)).frame(width: 420, height: 360, alignment: .center)
+        } label: {
+        Image("Hades")
+        .resizable()
+        .scaledToFit()
+        .frame(width: 240, height: 135)
+        }
+
+        NavigationLink {
+        VideoPlayer(player: AVPlayer(url: URL(string: urlVideos[4])!)).frame(width: 420, height: 360, alignment: .center)
+        } label: {
+        Image("Cuphead")
+        .resizable()
+        .scaledToFit()
+        .frame(width: 240, height: 135)
+
+        }
+
+        }
+
+        }
+        
+        NavigationLink(destination: VideoPlayer(player: AVPlayer(url: URL(string: url)!)), isActive: $isPlayerActive,
+            label: {EmptyView()})
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
+
